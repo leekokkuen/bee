a528f5992260aff2d8c21d770bd26167f68c0940
leekokkuen <84341838@qq.com>
2016-04-24 00:43:45 +0800

    fgfg

--- src/main/java/bee/controller/LoginController.java
+++ src/main/java/bee/controller/LoginController.java
@@ -1,5 +1,7 @@
 package bee.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,10 +18,12 @@ public class LoginController{
 	 * @return
 	 */
     @RequestMapping("/doLogin.ajax")
-	public @ResponseBody JsonResultInfo login(String user, String password) {
+	public @ResponseBody JsonResultInfo login(String user, String password,HttpServletRequest request) {
     	System.out.println(user+password);
-		return new JsonResultInfo(0,"","成功");
-		
-		//return new JsonResultInfo(1,"用户名或密码不正确","");
+    	if("liguoquan".equals(user)&&"aaa111".equals(password)){
+    		request.getSession().setAttribute("user", "OK");
+    		return new JsonResultInfo(0,"","成功");
+    	}
+    	return new JsonResultInfo(1,"用户名或密码不正确","");
 	}
 }
